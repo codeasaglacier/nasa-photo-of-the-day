9b09a5262c8fb0b8d42084b7a2426cfe2c7ada4d
codeasaglacier <remingtonshock@gmail.com>
2019-10-29 12:57:13 -0800

    APOD added

--- src/App.js
+++ src/App.js
@@ -5,28 +5,30 @@ import axios from 'axios';
 
 
 function App() {
+  
+  const [photo, setPhoto] = useState('')
+  console.log(`Photo: ${photo}`)
 
   useEffect(() => {
     axios
       .get('https://api.nasa.gov/planetary/apod?api_key=6FjJUz3MqO15yZ0pVuxG8P3rs5mhdnFMjMf2tgHA')
       .then(response => {
-        console.log(`Response: ${response.data}`)
-        
+        console.log(`Response: ${JSON.stringify(response.data.hdurl)}`)
+        setPhoto(response.data.hdurl)
       })
       .catch(error => {
         console.log(`Error: ${error}`)
       })
   }, [])
 
-  const [photo, setPhoto] = useState([])
-  console.log(`Photo: ${photo}`)
 
   return (
     <div className="App">
 
 
       <h2 className='title'>NASA Astronomy Photo Of the Day</h2>
-      
+      <img className='pika' src='https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png' alt='Shocked Pikachu'/>
+      <img className='photo' src={photo} alt="pretty space stuff"/>
     </div>
   );
 }
