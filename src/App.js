de3e4da1f2da8b4d1f60403fdd01ba12a8617cd6
codeasaglacier <remingtonshock@gmail.com>
2019-10-30 18:35:07 -0800

    set nasa apod to random date

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect }from "react";
 import "./App.css";
 import axios from 'axios';
 import Styled from 'styled-components';
+import DateGenerator from 'random-date-generator';
 
 const Page = Styled.div`
 text-shadow: 0 0 2px #fff,
@@ -33,11 +34,29 @@ function App() {
   const [photo, setPhoto] = useState('')
   console.log(`Photo: ${photo}`)
 
+  
   useEffect(() => {
+    const date = () => {
+
+    const utc = new Date().toJSON().slice(0,10);
+    // document.write(utc);
+    console.log(`UTC: ${utc}`)
+
+    const startDate = new Date(2006, 7, 4);
+    const endDate = new Date(2019, 10, 30);
+
+    let randomDate = DateGenerator.getRandomDateInRange(startDate, endDate)
+    
+    return(randomDate.toJSON().slice(0,10))
+    }
+
+    
+
     axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=6FjJUz3MqO15yZ0pVuxG8P3rs5mhdnFMjMf2tgHA')
+      .get(`https://api.nasa.gov/planetary/apod?api_key=6FjJUz3MqO15yZ0pVuxG8P3rs5mhdnFMjMf2tgHA&date=${date()}`)
       .then(response => {
-        console.log(`Response: ${JSON.stringify(response.data.hdurl)}`)
+        console.log(`Response URL: ${JSON.stringify(response.data.hdurl)}`)
+        console.log(`Response: ${JSON.stringify(response.data)}`)
         setPhoto(response.data.hdurl)
       })
       .catch(error => {
@@ -49,7 +68,7 @@ function App() {
   return (
     <div className="App">
       <Page>
-        <h2 className='title'>NASA Astronomy Photo Of the Day</h2>
+        <h2 className='title'>codeasaglacier's 'NASA Astronomy Photo Of the Day</h2>
         <Pika src='https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png' alt='Shocked Pikachu'/>
         <APOD src={photo} alt="pretty space stuff"/>
       </Page>
