e62ab1f57599e32ff1ee5ae91d51498388a77a91
codeasaglacier <remingtonshock@gmail.com>
2020-01-09 17:09:23 -0900

    added date and explanation

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Styled from 'styled-components';
 import DateGenerator from 'random-date-generator';
 
 const Page = Styled.div`
-text-shadow: 0 0 2px #fff,
-1px 1px 2px #fff,
-0 1px 2px #fff,
-1px 0 2px #fff;
+
 `
+
+// const Title = Styled.div`
+// background: rgba(0, 0, 0, 0.0);
+// `
+// text-shadow: 0 0 2px #fff,
+// 1px 1px 2px #fff,
+// 0 1px 2px #fff,
+// 1px 0 2px #fff;
+
 const Pika = Styled.img`
 position: absolute;
 top: 0;
@@ -26,21 +32,33 @@ left: 0;
 z-index: -1;
 height: 100vh;
 width: 100vw;
-
 `
+const About = Styled.div`
+position: fixed;
+bottom: 0;
+display: flex;
+justify-content: center;
+flex-direction: column;
+text-shadow: -1px -1px 2px #fff,
+1px 1px 2px #fff,
+1px -1px 2px #fff,
+-1px 1px 2px #fff;
+`
+
 
 function App() {
   
-  const [photo, setPhoto] = useState('')
-  console.log(`Photo: ${photo}`)
+  const [ photo, setPhoto ] = useState('')
+  // console.log( `Photo: ${photo}` )
 
+  const [ about, setAbout ] = useState('')
+  // console.log( `About: ${about} `)
+
+  const [ date, setDate ] = useState('')
+  // console.log( `Date: ${date}`)
   
   useEffect(() => {
     const date = () => {
-
-    const utc = new Date().toJSON().slice(0,10);
-    // document.write(utc);
-    console.log(`UTC: ${utc}`)
 
     const startDate = new Date(2006, 7, 4);
     const endDate = new Date(2019, 10, 30);
@@ -58,6 +76,8 @@ function App() {
         console.log(`Response URL: ${JSON.stringify(response.data.hdurl)}`)
         console.log(`Response: ${JSON.stringify(response.data)}`)
         setPhoto(response.data.hdurl)
+        setDate(response.data.date)
+        setAbout(response.data.explanation)
       })
       .catch(error => {
         console.log(`Error: ${error}`)
@@ -68,9 +88,15 @@ function App() {
   return (
     <div className="App">
       <Page>
-        <h2 className='title'>codeasaglacier's NASA Astronomy Photo Of the Day</h2>
+        <div className = 'title'>
+         <h2>codeasaglacier's NASA Astronomy Photo Of the Day</h2>
+        </div>
         <Pika src='https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png' alt='Shocked Pikachu'/>
         <APOD src={photo} alt="pretty space stuff"/>
+        <About>
+          <p>{date}</p>
+          <p>{about}</p>
+        </About>
       </Page>
     </div>
   );
